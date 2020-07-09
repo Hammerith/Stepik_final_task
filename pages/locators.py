1e8da807170695456b6be474e3af4118b398ee01
Hammerith <hammerith@yandex.ru>
2020-07-09 17:51:38 +0300

    added BasketPageLocators and BASKET_BUTTON

--- pages/locators.py
+++ pages/locators.py
@@ -21,3 +21,9 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini .btn-default")
+
+
+class BasketPageLocators():
+    BASKET_ITEMS = (By.CSS_SELECTOR, "div.basket-items")
+    BASKET_EMPTY_MSG = (By.CSS_SELECTOR, "#content_inner")
