dc63ba65925fe2379e3b3076884c50e3b04f0d9f
Hammerith <hammerith@yandex.ru>
2020-07-09 17:52:44 +0300

    added test for basket

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import time
 import pytest
 
@@ -41,17 +42,27 @@ import pytest
 #     page.open()
 #     page.add_to_basket()
 #     page.message_disappeared()
+#
+#
+# def test_guest_should_see_login_link_on_product_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+#
+#
+# def test_guest_can_go_to_login_page_from_product_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
 
 
-def test_guest_should_see_login_link_on_product_page(browser):
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_login_link()
-
-
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.no_product_in_basket()
+    basket_page.basket_empty_message()
