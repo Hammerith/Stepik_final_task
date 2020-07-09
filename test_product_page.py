5b766ee9c23c7325c9e339ba095afaa672d80b8e
Hammerith <hammerith@yandex.ru>
2020-07-09 12:40:12 +0300

    added parametrize and xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,18 @@
 from .pages.product_page import ProductPage
 import time
+import pytest
 
 
-def test_add_to_basket_get_code(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_add_to_basket_get_code(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_20" \
+           f"7/?promo=offer{link}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_add_message()
     page.basket_equal_product_price()
-
+    # time.sleep(120)
