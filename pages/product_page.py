32595c188ddef12c78500e2864b5a2df650a90e4
Hammerith <hammerith@yandex.ru>
2020-07-09 12:39:31 +0300

    changed asserts

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,9 +10,9 @@ class ProductPage(BasePage):
     def should_be_add_message(self):
         book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
         success_msg = self.browser.find_element(*ProductPageLocators.SUCCESS_MSG).text
-        assert book_name in success_msg, "wrong book name"
+        assert book_name == success_msg, "wrong book name"
 
     def basket_equal_product_price(self):
         book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
         basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
-        assert book_price in basket_price, "wrong price"
+        assert book_price == basket_price, "wrong price"
